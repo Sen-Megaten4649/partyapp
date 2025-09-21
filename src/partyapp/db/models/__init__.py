c57690034d61b0dd948f2bee561bbbbc3e485bb2
Sen <ffix.3000.c@gmail.com>
2025-09-21 19:53:17 +0900

    モデル定義 init.py

--- src/partyapp/db/models/__init__.py
+++ src/partyapp/db/models/__init__.py
@@ -2,5 +2,13 @@ from .law import Law
 from .category import Category
 from .party import Party
 from .associations import LawCategoryMap, PartyLawRole
+from .enums import (
+    LAW_TYPE_VALUES, JURISDICTION_VALUES,
+    SUBMISSION_ROLE_VALUES, PROMOTION_ROLE_VALUES, VOTE_ROLE_VALUES,
+)
 
-__all__ = ["Law", "Category", "Party", "LawCategoryMap", "PartyLawRole"]
+__all__ = [
+    "Law", "Category", "Party", "LawCategoryMap", "PartyLawRole",
+    "LAW_TYPE_VALUES", "JURISDICTION_VALUES",
+    "SUBMISSION_ROLE_VALUES", "PROMOTION_ROLE_VALUES", "VOTE_ROLE_VALUES",
+]
